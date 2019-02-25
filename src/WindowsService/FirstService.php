9a93d473cd3954953a39e9e8bcce5b8c942b3355
Jean-Baptiste NAHAN <macintoshplus@users.noreply.github.com>
2019-02-25 18:04:52 +0100

    first commit

---
+++ src/WindowsService/FirstService.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @copy Win32Service (c) 2019
+ * Added by : cameleon at 25/02/19 17:51
+ */
+namespace App\WindowsService;
+use \Win32Service\Model\AbstractServiceRunner;
+/**
+ * Class FirstService
+ *
+ */
+class FirstService extends AbstractServiceRunner
+{
+    protected function run(int $control): void
+    {
+        usleep(500);
+    }
+
+    protected function setup(): void
+    {
+        // TODO: Implement setup() method.
+    }
+
+    protected function beforePause(): void
+    {
+        // TODO: Implement beforePause() method.
+    }
+
+    protected function beforeContinue(): void
+    {
+        // TODO: Implement beforeContinue() method.
+    }
+
+    protected function beforeStop(): void
+    {
+        // TODO: Implement beforeStop() method.
+    }
+
+    protected function lastRunIsTooSlow(float $duration): void
+    {
+        // TODO: Implement lastRunIsTooSlow() method.
+    }
+
+}
